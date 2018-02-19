0c79036bf34a183acd5eb3f20d527e6a05f530a1
kahneraja <kahneraja@gmail.com>
2018-02-18 19:06:45 -0500

    wip

--- src/components/github/authorization/Grant.js
+++ src/components/github/authorization/Grant.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Grant extends Component {
     render() {
         let client_id = process.env.REACT_APP_GITHUB_CLIENT_ID
-        let redirect_uri = `${window.location.host}/github/authorization/token`
+        let redirect_uri = `${window.location.protocol}//${window.location.host}/github/authorization/token`
         let authorizeUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,read:org,read:user&redirect_uri=${redirect_uri}`
         return (
             <div>
