5463fc5939ac094303a00ba7e527bee3a9ad9dfc
kahneraja <kahneraja@gmail.com>
2018-02-18 19:10:11 -0500

    wip

--- src/components/slack/authorization/Grant.js
+++ src/components/slack/authorization/Grant.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Grant extends Component {
     render() {
         let client_id = process.env.REACT_APP_SLACK_CLIENT_ID
-        let redirect_uri = `${window.location.host}/slack/authorization/token`
+        let redirect_uri = `${window.location.protocol}//${window.location.host}/slack/authorization/token`
         let authorizeUrl = `https://slack.com/oauth/authorize?client_id=${client_id}&scope=chat:write:bot,users:read&redirect_uri=${redirect_uri}`
         return (
             <div>
